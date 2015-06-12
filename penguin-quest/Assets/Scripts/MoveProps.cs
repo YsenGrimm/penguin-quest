5c90226a253925212ec3c72b1dfe4e9a9822b349
Asema Hassan <asema.hassan@gmail.com>
2015-06-12 20:58:27 +0200

    Updated game structure
    2d scrolling basic, some basic props for testing.

---
+++ penguin-quest/Assets/Scripts/MoveProps.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveProps : MonoBehaviour {
+
+	public Vector2 velocity = new Vector2(-3, 0);
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().velocity=velocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ penguin-quest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	public Rigidbody2D penguin;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(Input.GetKeyUp("space"))
+		{
+			penguin.velocity=Vector2.zero;
+			penguin.AddForce(new Vector2(0,250));
+		}
+
+	}
+
+
+}
