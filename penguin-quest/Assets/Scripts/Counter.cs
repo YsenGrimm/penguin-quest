166fec4f501b909b40130d36bf6ae05bca8ea58c
Asema Hassan <asema.hassan@gmail.com>
2015-06-12 22:13:57 +0200

    Update gameplay
    Added egg controller, counter and auto destroy script .

---
+++ penguin-quest/Assets/Scripts/Counter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Counter : MonoBehaviour {
+
+	int counter =0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void IncrementCounterByOne(){
+		counter++;
+		this.GetComponent<Text> ().text = counter.ToString ();
+	}
+}
