8e52b3ca58286575032251607e9d7611daaa1ef1
Asema Hassan <asema.hassan@gmail.com>
2015-06-13 00:39:51 +0200

    Added Random Clouds
    changed background, added clouds animation, fixed water animation.

---
+++ penguin-quest/Assets/Scripts/CloudGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CloudGenerator : MonoBehaviour {
+	
+	public List<GameObject> clouds = new List<GameObject>(); 
+	GameObject selectedCloud;
+	int randomIndex;
+	Vector3 startPos = new Vector3(10.0f,4.0f,0.0f);
+	Vector3 endPos = new Vector3(-15.0f,4.0f,0.0f);
+	// Use this for initialization
+	
+	void destroyCloud(Object obj){
+		Destroy((GameObject)obj);
+	}
+	void createClouds(){
+		randomIndex = Random.Range(0,clouds.Count);
+		selectedCloud = Instantiate(clouds[randomIndex],startPos,Quaternion.identity) as GameObject;
+		iTween.MoveTo(selectedCloud,iTween.Hash("position",endPos,"time",30.0f,"onComplete","destroyCloud",
+		                                        "oncompletetarget", gameObject,"oncompleteparams",selectedCloud,"easetype",iTween.EaseType.linear));
+		Invoke("createClouds",20.0f);
+	}
+	void Start (){
+		createClouds();
+		
+	}
+}
