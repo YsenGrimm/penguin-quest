143857357497d69bc8bfdc0bf79393a62df66c6f
Asema Hassan <asema.hassan@gmail.com>
2015-06-13 23:28:25 +0200

    Random generation fixed
    more speed, checking distance. Death with waves disabled.

--- penguin-quest/Assets/Scripts/Counter.cs
+++ penguin-quest/Assets/Scripts/Counter.cs
@@ -5,16 +5,6 @@ using System.Collections;
 public class Counter : MonoBehaviour {
 
 	int counter =0;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public void IncrementCounterByOne(){
 		counter++;
 		this.GetComponent<Text> ().text = counter.ToString ();
---
+++ penguin-quest/Assets/Scripts/SOA_ChangeSortingLayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SOA_ChangeSortingLayer : MonoBehaviour {
+
+
+	public string sortingLayerName;
+	// Use this for initialization
+	void Awake () {
+
+		SpriteRenderer myRenderer =  this.gameObject.GetComponent<SpriteRenderer>();
+		if(myRenderer != null)
+			myRenderer.sortingLayerName = sortingLayerName;
+	
+	}
+}
