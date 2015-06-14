7f9dd151c4d848c82f0a2e85568d6cabe1c1e54e
YsenGrimm <kloeten.wars@googlemail.com>
2015-06-14 15:26:10 +0200

    Menu now with working buttons.

---
+++ penguin-quest/Assets/Scripts/MenuController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+	
+	public GameObject HowToOverlay;
+	public GameObject AboutOverlay;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void PlayGame () {
+		Application.LoadLevel("GamePlay_Level1");
+	}
+	
+	public void DisplayHowTo() {
+		HowToOverlay.SetActive(true);
+	}
+	
+	public void DisplayAbout() {
+		AboutOverlay.SetActive(true);
+	}
+	
+	public void HideHowTo() {
+		HowToOverlay.SetActive(false);
+	}
+	
+	public void HideAbout() {
+		AboutOverlay.SetActive(false);
+	}
+	
+	public void QuitGame() {
+		Application.Quit();
+	}
+	
+	public void SwitchToHighscore() {
+		Application.LoadLevel("Highscore");
+	}
+}
