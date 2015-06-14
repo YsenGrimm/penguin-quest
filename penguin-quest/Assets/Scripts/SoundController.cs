9d4dda3219ca2b2cb35069420c758f4702ae9e90
Asema Hassan <asema.hassan@gmail.com>
2015-06-14 14:58:50 +0200

    Sounds effects added

---
+++ penguin-quest/Assets/Scripts/SoundController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	public AudioClip jumpSfx1,jumpSfx2,celebSfx,waterSplash,dying;
+	public AudioSource aSource;
+	
+	public void playSoundEffect(string _name){
+		
+		if(_name=="Jump1")
+			aSource.clip = jumpSfx1;
+		if(_name=="Jump2")
+			aSource.clip = jumpSfx2;
+		else if(_name=="Celeb")
+			aSource.clip = celebSfx;
+		else if(_name=="Water")
+			aSource.clip = waterSplash;
+		else if(_name=="Lose")
+			aSource.clip = dying;
+		
+		aSource.Play();
+	}
+
+}
