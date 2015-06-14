3706d96bda5752076f69d3748ad92f6438978c1f
YsenGrimm <kloeten.wars@googlemail.com>
2015-06-14 16:46:33 +0200

    Highscore

---
+++ penguin-quest/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void PlayAgain() {
+		Application.LoadLevel("GamePlay_Level1");
+	}
+	
+	public void BackToMenu() {
+		Application.LoadLevel("MainMenu");
+	}
+}
