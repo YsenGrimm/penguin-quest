8f6a093095fbadebc0655b69b64df5b4d05e2f8c
YsenGrimm <kloeten.wars@googlemail.com>
2015-06-14 17:05:42 +0200

    Displays the correct highscore.

--- penguin-quest/Assets/Scripts/ScoreController.cs
+++ penguin-quest/Assets/Scripts/ScoreController.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ScoreController : MonoBehaviour {
+	
+	public Text ScoreText;
 
 	// Use this for initialization
 	void Start () {
-	
+		float time 		= PlayerPrefs.GetFloat("Time");
+		int eggScore 	= PlayerPrefs.GetInt("Eggs");
+		int minutes = (int)time / 60;
+		int seconds = (int)time % 60;
+		
+		ScoreText.text = "The best player survied \n" + 
+							string.Format("{0:00} : {1:00}", minutes, seconds) +
+							" minutes and \ncollected " + eggScore.ToString() + " Eggs";
 	}
 	
 	// Update is called once per frame
